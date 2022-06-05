b060fef9c18a0ca795310aafe2929b3e62afa2d9
AhnafAhamed <ahnafahamed4@gmail.com>
2022-06-05 17:18:25 +0530

    fix: move colors to extend object

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,17 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "light-purple": "#F4F3FA",
+        "dark-purple": "#4C49ED",
+        "main-gray": "#A1A0BD",
+        "main-black": "#000000",
+        "main-red": "#D14F4F",
+      },
+    },
     fontFamily: {
       inter: ["inter", "sans-serif"],
-    },
-    colors: {
-      "light-purple": "#F4F3FA",
-      "dark-purple": "#4C49ED",
-      "main-gray": "#A1A0BD",
-      "main-black": "#000000",
-      "main-red": "#D14F4F",
     },
   },
   plugins: [],
