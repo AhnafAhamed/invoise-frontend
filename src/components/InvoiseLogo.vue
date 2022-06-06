666a61b261e9b4f252285d1d4afb3367a596f42f
AhnafAhamed <ahnafahamed4@gmail.com>
2022-06-06 07:47:37 +0530

    create invoise logo

---
+++ src/components/InvoiseLogo.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="logo flex items-center">
+    <invoice-icon class="mr-2" />
+    <p class="font-extrabold text-2xl tracking-wider">
+      IN<span class="text-dark-purple tracking-tight">VOISE</span>
+    </p>
+  </div>
+</template>
+
+<script>
+import InvoiceIcon from "./icons/InvoiceIcon.vue";
+export default {
+  components: { InvoiceIcon },
+  name: "InvoiseLogo",
+};
+</script>
+
+<style></style>
