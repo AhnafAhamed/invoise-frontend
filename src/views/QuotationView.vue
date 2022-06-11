77deac9b09d07e980287f1fbad8fc876c892a514
AhnafAhamed <ahnafahamed4@gmail.com>
2022-06-11 11:55:38 +0530

    feat: add table to quotation view

--- src/views/QuotationView.vue
+++ src/views/QuotationView.vue
@@ -1,13 +1,22 @@
 <template>
-  <div class="quotation">
+  <div class="quotation flex">
     <side-bar />
+    <div class="mt-16 ml-16">
+      <div class="flex justify-between items-end">
+        <h1 class="text-2xl font-extrabold uppercase">Quotations</h1>
+        <add-button :text="'New Quotation'" />
+      </div>
+      <main-table class="mt-12" />
+    </div>
   </div>
 </template>
 
 <script>
+import AddButton from "../components/AddButton.vue";
+import MainTable from "../components/MainTable.vue";
 import SideBar from "../components/SideBar.vue";
 export default {
-  components: { SideBar },
+  components: { SideBar, MainTable, AddButton },
   name: "QuotationView",
 };
 </script>
