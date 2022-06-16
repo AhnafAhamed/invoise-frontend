50cd3204ea495c47634c198bf1c790e9db1855ca
AhnafAhamed <ahnafahamed4@gmail.com>
2022-06-16 19:25:23 +0530

    fix: modal backdrop color and opacity

--- src/components/FormModal.vue
+++ src/components/FormModal.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="flex m-auto bg-gray-600 overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 w-full md:inset-0 h-modal md:h-full justify-center items-center"
+    class="flex m-auto bg-gray-900 bg-opacity-50 dark:bg-opacity-80 overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 w-full md:inset-0 h-modal md:h-full justify-center items-center"
   >
     <div class="relative p-4 w-full max-w-md h-full md:h-auto">
       <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
