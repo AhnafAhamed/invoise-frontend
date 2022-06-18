6b1e02d8524cb274880b9d1fc466b31ce7554f27
AhnafAhamed <ahnafahamed4@gmail.com>
2022-06-18 10:22:51 +0530

    feat: style settings view

--- src/components/settings/AppSettings.vue
+++ src/components/settings/AppSettings.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="app-settings">
-    <form class="mt-8">
+    <form class="mt-8 w-3/4 m-auto">
       <label
         for="name"
         class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
@@ -15,7 +15,7 @@
       />
       <label
         for="address"
-        class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+        class="block mb-2 mt-6 text-sm font-medium text-gray-900 dark:text-gray-300"
         >Your Address</label
       >
       <input
@@ -25,6 +25,12 @@
         placeholder="address"
         required
       />
+      <button
+        type="submit"
+        class="block w-2/4 m-auto mt-6 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+      >
+        Change
+      </button>
     </form>
   </div>
 </template>
--- src/components/settings/ProfileSettings.vue
+++ src/components/settings/ProfileSettings.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="profile-settings">
-    <form class="mt-8">
+    <form class="mt-8 w-3/4 m-auto">
       <label
         for="name"
         class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
@@ -15,7 +15,7 @@
       />
       <label
         for="password"
-        class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+        class="block mb-2 mt-6 text-sm font-medium text-gray-900 dark:text-gray-300"
         >Password</label
       >
       <input
@@ -25,6 +25,12 @@
         placeholder="Password"
         required
       />
+      <button
+        type="submit"
+        class="block w-2/4 m-auto mt-6 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+      >
+        Change
+      </button>
     </form>
   </div>
 </template>
