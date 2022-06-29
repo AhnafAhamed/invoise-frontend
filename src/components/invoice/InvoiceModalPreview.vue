358ec799b3f0820507ee384f1280d4dc61b23a7e
AhnafAhamed <ahnafahamed4@gmail.com>
2022-06-29 07:12:29 +0530

    feat: create preview for modals

---
+++ src/components/invoice/InvoiceModalPreview.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="invoice-modal-preview"></div>
+</template>
+
+<script>
+export default {
+  name: "InvoiceModalPreview",
+};
+</script>
+
+<style></style>
---
+++ src/components/quotation/QuotationModalPreview.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="invoice-modal-preview"></div>
+</template>
+
+<script>
+export default {
+  name: "QuotationModalPreview",
+};
+</script>
+
+<style></style>
